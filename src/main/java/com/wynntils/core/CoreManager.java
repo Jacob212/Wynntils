f6288f0599ca91fb502e96974341429d25e0c962
Jacob212 <jacobhale.hale@gmail.com>
2021-12-20 22:51:50 +0000

    moved ClientEvents register away from this file

--- src/main/java/com/wynntils/core/CoreManager.java
+++ src/main/java/com/wynntils/core/CoreManager.java
@@ -5,11 +5,8 @@
 package com.wynntils.core;
 
 import com.wynntils.McIf;
-import com.wynntils.core.events.ClientEvents;
 import com.wynntils.core.framework.enums.wynntils.WynntilsConflictContext;
 import com.wynntils.core.framework.instances.PlayerInfo;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.common.MinecraftForge;
 
 public class CoreManager {
 
@@ -17,7 +14,6 @@ public class CoreManager {
      * Called before all modules are registered
      */
     public static void preModules() {
-        MinecraftForge.EVENT_BUS.register(new ClientEvents());
 
         McIf.mc().options.keyUp.setKeyConflictContext(WynntilsConflictContext.ALLOW_MOVEMENTS);
         McIf.mc().options.keyDown.setKeyConflictContext(WynntilsConflictContext.ALLOW_MOVEMENTS);
